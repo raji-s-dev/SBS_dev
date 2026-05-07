380c3bb8713f53c9b381dce82bc575a558cf1a33
raji-s-dev <rajisivappragasam@gmail.com>
2026-05-07 14:19:11 +0530

    SBS-initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+## Required Environment
+
+- Node.js 20.17.x (LTS)
+- npm 10.x
+
+We recommend using NVM.
+
+Check version:
+
+node -v
+npm -v
+
+
+## Setup
+
+1. Install NVM
+2. Run: nvm use
+3. Run: npm install
+4. Run: npm run dev
---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+// src/app/App.tsx
+
+
+
+export default function App() {
+  return (
+   
+      <div className="flex min-h-screen items-center justify-center">
+        <h1 className="text-3xl font-bold">Hello World</h1>
+      </div>
+  
+  )
+}
